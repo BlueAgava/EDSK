1fecc8468da39e5620accc61a0258a71ab1d49a6
BlueAgava <smileofcactus@gmail.com>
2021-12-20 18:11:47 +0300

    табличка

--- table/js/input.js
+++ table/js/input.js
@@ -1,8 +1,32 @@
 const inputForm = document.getElementById('#inputForm');
 
-const inputedEmail = document.querySelector("#inputedEmail");
-const inputedName = document.querySelector("#inputedName");
-const inputedText = document.querySelector("#inputedText");
-const inputedCheck = document.querySelector("#checkbox");
+// const inputedEmail = document.querySelector("#inputedEmail");
+// const inputedName = document.querySelector("#inputedName");
+// const inputedText = document.querySelector("#inputedText");
+// const inputedCheck = document.querySelector("#checkbox");
 
-console.log(inputedEmail, inputedName, inputedText, inputedCheck);
+const inputedEmail = document.querySelector("#inputForm input[name='inputed-email']").parentNode;
+const inputedName = document.querySelector("#inputForm input[name='inputed-name']").parentNode;
+const inputedReview = document.querySelector("#inputForm input[name='inputed-review']").parentNode;
+
+console.log(inputedEmail, inputedName);
+
+
+function initialazeForm(field){
+    const input = field.getElementByTagName("input")[0];
+    input.value = "";
+}
+
+const emailField = initialazeForm (inputedEmail);
+const nameField = initialazeForm (inputedName);
+const reviewField = initialazeForm (inputedReview);
+
+inputForm.addEventListener('submit', function (event){
+    event.preventDefault(); //перехватывает submit и форма срабатывает на ENTER
+    return{
+        getValue() {
+            return input.value
+        }
+    }
+    
+});
